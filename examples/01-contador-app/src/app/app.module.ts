64f8d01fdc8094eed7ce338aecba413af5f85f92
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-09-20 22:40:23 -0500

    Creación de Estado inicial, acciones y reducer de contador

--- examples/01-contador-app/src/app/app.module.ts
+++ examples/01-contador-app/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { environment } from '../environments/environment.prod';
+import { counterReducer } from './store/reducers/contador.reducer';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { environment } from '../environments/environment.prod';
   imports: [
     BrowserModule,
     ComponentsModule,
-    StoreModule.forRoot({ contador: null }),
+    StoreModule.forRoot({ contador: counterReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
